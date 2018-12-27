897c54effbb91b1100d4b3c7fcfc625155c066d3
Galaxyserver <46113872+Galaxyserver@users.noreply.github.com>
2018-12-27 23:08:33 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,33 +1,45 @@
-const prefix = "G";
-const Discord = require("discord.js");
-const client = new Discord.Client();
+client.on('message', message => {
+              if(!message.channel.guild) return;
+    var prefix = "G";
+    if(message.content.startsWith(prefix + 'bc')) {
+    if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
+  if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
+    let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
+    let copy = "Dragon";
+    let request = `Requested By ${message.author.username}`;
+    if (!args) return message.reply('```**يجب عليك كتابة كلمة او جملة لإرسال البرودكاست**```');message.channel.send(`**هل أنت متأكد من إرسالك البرودكاست؟ \nمحتوى البرودكاست:** \` ${args}\``).then(msg => {
+    msg.react('✅')
+    .then(() => msg.react('❌'))
+    .then(() =>msg.react('✅'))
 
-client.on("message", message => {
-let args = message.content.split(" ")
-if(args[0].toLowerCase() === (prefix + `bc`)){
-// let sdsa = message.content.split(" ").slice(6);
-if(!args[1]) {
-    return message.channel.send(`${message.author}, There's no message to send!`);
-} 
-if(!message.member.hasPermission("ADMINISTRATOR")){
-    return message.channel.send(`You have no perms to do this`);
-}
-        message.guild.members.forEach(f => {
-         
-            let embedbc = new Discord.RichEmbed;
-               embedbc.setTitle(`A new message!`);
-            embedbc.addField(`The server`, `${message.guild.name}`,true);
-            embedbc.addField(`The sender`,`${message.author}`,true);
-            embedbc.addField(`The message`, `${message.content.split(" ").slice(1).join(" ")}`,true);
-            embedbc.setFooter(`${galaxy.user.username}`, `${galaxy.user.displayAvatarURL}`);
-            embedbc.setThumbnail(`${message.author.displayAvatarURL}`);
-            embedbc.setColor(`RANDOM`);
-            f.send(embedbc);
-          
-        });
-       
-  message.channel.send(`**عدد الاعضاء المستلمين ${message.guild.members.filter(f => f.status !== 'offline').size}**\n **عدد الاعضاء الغير مستلمين${message.guild.members.filter(f => f.status === 'offline').size}**`);
-}
-});
+    let reaction1Filter = (reaction, user) => reaction.emoji.name === '✅' && user.id === message.author.id;
+    let reaction2Filter = (reaction, user) => reaction.emoji.name === '❌' && user.id === message.author.id;
+
+    let reaction1 = msg.createReactionCollector(reaction1Filter, { time: 12000 });
+    let reaction2 = msg.createReactionCollector(reaction2Filter, { time: 12000 });
+    reaction1.on("collect", r => {
+    message.channel.send(`☑ | Done ... The Broadcast Message Has Been Sent For ${message.guild.members.size} Members`).then(m => m.delete(5000));
+    message.guild.members.forEach(m => {
+    var bc = new
+       Discord.RichEmbed()
+       .setColor('RANDOM')
+       .setTitle('Broadcast')
+       .addField('Server', message.guild.name)
+       .addField('Sender', message.author.username)
+       .addField('Message', args)
+       .setThumbnail(message.author.avatarURL)
+       .setFooter(copy, client.user.avatarURL);
+    m.send({ embed: bc })
+    msg.delete();
+    })
+    })
+    reaction2.on("collect", r => {
+    message.channel.send(`**Broadcast Canceled.**`).then(m => m.delete(5000));
+    msg.delete();
+    })
+    })
+    }
+    });
+
 
 client.login(process.env.BOT_TOKEN);
