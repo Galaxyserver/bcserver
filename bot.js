9042953efffccd25d1f086d35d2fe83c97f03b01
Galaxyserver <46113872+Galaxyserver@users.noreply.github.com>
2018-12-28 02:35:31 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,5 +27,16 @@ message.guild.members.forEach(m => {
   }
  
 });
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By :Veerz`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : Veerz ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`Galaxy Server.`,"http://twitch.tv/S-F")
+client.user.setStatus("dnd")
+});
+
 
 client.login(process.env.BOT_TOKEN);
